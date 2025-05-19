f8dca6c4ebd86e32972dd4ee17b90ce02247aa71
tony <starreach33@gmail.com>
2025-05-18 22:55:07 -0600

    Fix :bug: Use bd configuration within index.js file

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const bodyParser = require('body-parser');
 
 const cors = require('cors');
 const mysql = require('mysql2/promise');
-const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'itamedic',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+// const pool = mysql.createPool({
+//   host: '127.0.0.1',
+//   user: 'root',
+//   password: '',
+//   database: 'itamedic',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+const pool = require('./src/configuraciones/bd');
 
 
 const rutasInicio = {
